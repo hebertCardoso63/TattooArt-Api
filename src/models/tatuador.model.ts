201f315e81678efad3839132997e58116fca3f10
hebertCardoso63 <cardosohebert34|@gmail.com>
2024-07-30 22:02:28 -0300

    # Models Tatuador

---
+++ src/models/tatuador.model.ts
@@ -0,0 +1,27 @@
+export interface Tatuador {
+    id: string;
+    experiencia: number;
+    status?: string;
+    tipo?: string;
+    redes_sociais?: Record<string, string>;
+    data_criacao?: Date;
+    data_atualizacao?: Date;
+    usuario_id: string;
+    estudio_id?: string;
+    imagem_perfil: string;
+    nome?: string;
+    estilo_tatuagem?: string[];
+    imagem_capa?: string;
+}
+
+export interface DadosCadastraisTatuador {
+    usuario_id: string;
+    nome: string;
+    experiencia: number;
+    status: string;
+    tipo: string;
+    imagem_perfil: string;
+    estudio_id?: string;
+    estilo_tatuagem?: string[];
+    redes_sociais?: Record<string, string>;
+}
