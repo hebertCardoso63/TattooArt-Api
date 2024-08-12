51e591c1c1323ddaa39596e94638788f22efe8f3
hebertCardoso63 <cardosohebert34|@gmail.com>
2024-08-12 03:06:30 -0300

    # Adicionando rota de tatuagem no arquivo principal de rotas

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import auth from './auth.route';
 import tatuador from './tatuador.route';
 import usuario from './usuario.route';
+import tatuagem from './tatuagem.route'
 import { validateAuth } from '../validators/auth.validator'
 import { authenticate } from '../middlewares/auth.middleware';
 
@@ -12,6 +13,7 @@ const router = Router();
 router
     .use('/auth', validateAuth, auth)
     .use('/api', authenticate, tatuador)
-    .use('/api', authenticate, usuario);
+    .use('/api', authenticate, usuario)
+    .use('/api', authenticate, tatuagem);
 
 export default router;
