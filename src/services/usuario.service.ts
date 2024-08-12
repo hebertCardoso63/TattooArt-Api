64fc1d04f940cf204a0ab6303d3c39987bf7521d
hebertCardoso63 <cardosohebert34|@gmail.com>
2024-08-11 21:20:27 -0300

    # Alterações no Service da rota de usuario

--- src/services/usuario.service.ts
+++ src/services/usuario.service.ts
@@ -1,7 +1,10 @@
 import knex from "../factories/knex.factory";
-import { DadosCadastrais, Usuario as UsuarioModel } from "../models/usuario.model";
+import { DadosCadastrais, UsuarioModel } from "../models/usuario.model";
+import { InternalError } from '../errors/internal.error';
+import { AtualizaPerfilInput } from '../types/usuario/atualiza-perfil.interface';
+import { Usuario } from '../types/usuario/usuario.entity';
 
-class Usuario {
+class UsuarioService {
     private async verificarExistencia(nomeUsuario: string): Promise<boolean> {
         const usuario = await this.encontrarUsuario(nomeUsuario)
         
@@ -36,6 +39,69 @@ class Usuario {
 
         return idUsuario.id;
     }
+
+    public async buscaContaUsuario(userId: string): Promise<Usuario> {
+        const response = await knex({ u: 'usuarios' })
+          .select(
+            'u.nome',
+            'u.email',
+            'u.cpf',
+            'u.rg',
+            'endereco',
+            'telefone_celular',
+            'data_criacao',
+          )
+          .where('id', userId)
+          .whereNull('data_exclusao')
+          .first()
+          .catch((e) => {
+            throw new InternalError(102, e.message);
+          });
+    
+        if (!response) {
+          throw new InternalError(101);
+        }
+    
+        return response;
+      }
+
+    public async excluiContaUsuario(userId: string): Promise<void> {
+        await knex({ u: 'usuarios' })
+          .update({ data_exclusao: knex.fn.now() })
+          .where('id', userId)
+          .whereNull('data_exclusao')
+          .catch((e) => {
+            throw new InternalError(103, e.message);
+          });
+      }
+
+    public async atualizaContaUsuario(
+        userId: string,
+        input: AtualizaPerfilInput,
+      ): Promise<Usuario> {
+        const query = knex({ u: 'usuarios' })
+          .update({
+            ...input,
+            data_atualizacao: knex.fn.now(),
+          })
+          .returning([
+            'u.nome',
+            'u.email',
+            'u.cpf',
+            'u.telefone_celular',
+            'u.rg',
+            'u.endereco',
+            'u.data_criacao',
+          ])
+          .where('id', userId)
+          .whereNull('data_exclusao');
+    
+        return query
+          .then((response) => response[0])
+          .catch((e) => {
+            throw new InternalError(104, e.message);
+          });
+      }
 }
 
-export const usuario = new Usuario();
+export const usuarioService = new UsuarioService();
