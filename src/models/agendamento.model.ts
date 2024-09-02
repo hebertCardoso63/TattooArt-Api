8236c33625fac5a55693b506e40b501ec413be77
hebertCardoso63 <cardosohebert34|@gmail.com>
2024-09-02 19:55:50 -0300

    # tatuagem id adicionado

--- src/models/agendamento.model.ts
+++ src/models/agendamento.model.ts
@@ -9,6 +9,7 @@ export interface AgendamentoModel {
     data_atualizacao: Date;
     data_exclusao: Date;
     cliente_id: string;
+    tatuagem_id: string;
     agendamento_id: string;
     tatuador_id: string;
     criado_por: string;
